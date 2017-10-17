8b38b7861bdce55a5330c52f4d0a2a650f49f765
dezhihuang <dezhihuang@foxmail.com>
2017-10-17 14:33:08 +0800

    修改程序名并上传测试文件

--- yuv420p_split.cpp
+++ yuv420p_split.cpp
@@ -6,17 +6,17 @@ typedef int BOOL;
 #define TRUE  1
 #define FALSE 0
 
-BOOL yuv420_split(const char *file, int width, int height)
+BOOL yuv420p_split(const char *file, int width, int height)
 {
     if (file == NULL) {
         return FALSE;
     }
     FILE *fp  = fopen(file, "rb+");
     
-    FILE *fp0 = fopen("output_420_yuv.y", "wb+");
-    FILE *fp1 = fopen("output_420_y.y", "wb+");
-    FILE *fp2 = fopen("output_420_u.y", "wb+");
-    FILE *fp3 = fopen("output_420_v.y", "wb+");
+    FILE *fp0 = fopen("./out/output_420p_yuv.y", "wb+");
+    FILE *fp1 = fopen("./out/output_420p_y.y",   "wb+");
+    FILE *fp2 = fopen("./out/output_420p_u.y",   "wb+");
+    FILE *fp3 = fopen("./out/output_420p_v.y",   "wb+");
     
     unsigned char *data = (unsigned char *)malloc(width*height*3/2);
     memset(data, 0, width*height*3/2);
@@ -40,7 +40,7 @@ BOOL yuv420_split(const char *file, int width, int height)
 int main(int argc, char *argv[])
 {
     if (argc == 4) {
-        yuv420_split(argv[1], atoi(argv[2]), atoi(argv[3]));
+        yuv420p_split(argv[1], atoi(argv[2]), atoi(argv[3]));
     }
     
     return 0;
